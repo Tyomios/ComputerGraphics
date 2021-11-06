758bc335781dea2653ca91e601942a0f3a16205c
Tyomios <tyomios@mail.ru>
2021-11-06 20:25:06 +0700

    Сделал постройку отрезка по координатам и мышкой

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,116 +29,210 @@ namespace ComputerGraphics
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.createFirstMartixButton = new System.Windows.Forms.Button();
-			this.createSecondMatrixButton = new System.Windows.Forms.Button();
-			this.resultButton = new System.Windows.Forms.Button();
-			this.saveResultButton = new System.Windows.Forms.Button();
-			this.matrixSizeLabel = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.firstMatrixStatusLabel = new System.Windows.Forms.Label();
-			this.secondMatrixStatusLabel = new System.Windows.Forms.Label();
+			this.clearButton = new System.Windows.Forms.Button();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.simpleCDARadioButton = new System.Windows.Forms.RadioButton();
+			this.pointDrawLabel = new System.Windows.Forms.Label();
+			this.panel = new System.Windows.Forms.Panel();
+			this.yKLabel = new System.Windows.Forms.Label();
+			this.xKLabel = new System.Windows.Forms.Label();
+			this.lineBuildButton = new System.Windows.Forms.Button();
+			this.yKtextBox = new System.Windows.Forms.TextBox();
+			this.xKtextBox = new System.Windows.Forms.TextBox();
+			this.yNLabel = new System.Windows.Forms.Label();
+			this.xNLabel = new System.Windows.Forms.Label();
+			this.yNTextBox = new System.Windows.Forms.TextBox();
+			this.xNTextBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.panel.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// createFirstMartixButton
+			// clearButton
 			// 
-			this.createFirstMartixButton.Location = new System.Drawing.Point(13, 39);
-			this.createFirstMartixButton.Name = "createFirstMartixButton";
-			this.createFirstMartixButton.Size = new System.Drawing.Size(136, 39);
-			this.createFirstMartixButton.TabIndex = 0;
-			this.createFirstMartixButton.Text = "Создать первую матрицу";
-			this.createFirstMartixButton.UseVisualStyleBackColor = true;
+			this.clearButton.Location = new System.Drawing.Point(79, 328);
+			this.clearButton.Name = "clearButton";
+			this.clearButton.Size = new System.Drawing.Size(136, 23);
+			this.clearButton.TabIndex = 2;
+			this.clearButton.Text = "Очистить";
+			this.clearButton.UseVisualStyleBackColor = true;
+			this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
 			// 
-			// createSecondMatrixButton
+			// pictureBox
 			// 
-			this.createSecondMatrixButton.Location = new System.Drawing.Point(12, 84);
-			this.createSecondMatrixButton.Name = "createSecondMatrixButton";
-			this.createSecondMatrixButton.Size = new System.Drawing.Size(137, 42);
-			this.createSecondMatrixButton.TabIndex = 1;
-			this.createSecondMatrixButton.Text = "Создать вторую матрицу";
-			this.createSecondMatrixButton.UseVisualStyleBackColor = true;
+			this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox.Location = new System.Drawing.Point(12, 28);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(351, 358);
+			this.pictureBox.TabIndex = 3;
+			this.pictureBox.TabStop = false;
+			this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+			this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
 			// 
-			// resultButton
+			// groupBox1
 			// 
-			this.resultButton.Location = new System.Drawing.Point(306, 150);
-			this.resultButton.Name = "resultButton";
-			this.resultButton.Size = new System.Drawing.Size(136, 23);
-			this.resultButton.TabIndex = 2;
-			this.resultButton.Text = "Результат";
-			this.resultButton.UseVisualStyleBackColor = true;
+			this.groupBox1.Controls.Add(this.simpleCDARadioButton);
+			this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.groupBox1.Location = new System.Drawing.Point(14, 20);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(254, 113);
+			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Выберите алгоритм";
 			// 
-			// saveResultButton
+			// simpleCDARadioButton
 			// 
-			this.saveResultButton.Location = new System.Drawing.Point(13, 132);
-			this.saveResultButton.Name = "saveResultButton";
-			this.saveResultButton.Size = new System.Drawing.Size(136, 41);
-			this.saveResultButton.TabIndex = 3;
-			this.saveResultButton.Text = "Сохранить результат";
-			this.saveResultButton.UseVisualStyleBackColor = true;
+			this.simpleCDARadioButton.AutoSize = true;
+			this.simpleCDARadioButton.Location = new System.Drawing.Point(6, 35);
+			this.simpleCDARadioButton.Name = "simpleCDARadioButton";
+			this.simpleCDARadioButton.Size = new System.Drawing.Size(108, 19);
+			this.simpleCDARadioButton.TabIndex = 3;
+			this.simpleCDARadioButton.TabStop = true;
+			this.simpleCDARadioButton.Text = "Обычный ЦДА";
+			this.simpleCDARadioButton.UseVisualStyleBackColor = true;
+			this.simpleCDARadioButton.CheckedChanged += new System.EventHandler(this.simpleCDARadioButton_CheckedChanged);
 			// 
-			// matrixSizeLabel
+			// pointDrawLabel
 			// 
-			this.matrixSizeLabel.AutoSize = true;
-			this.matrixSizeLabel.Location = new System.Drawing.Point(13, 13);
-			this.matrixSizeLabel.Name = "matrixSizeLabel";
-			this.matrixSizeLabel.Size = new System.Drawing.Size(30, 15);
-			this.matrixSizeLabel.TabIndex = 4;
-			this.matrixSizeLabel.Text = "Size:";
+			this.pointDrawLabel.AutoSize = true;
+			this.pointDrawLabel.Location = new System.Drawing.Point(14, 146);
+			this.pointDrawLabel.Name = "pointDrawLabel";
+			this.pointDrawLabel.Size = new System.Drawing.Size(120, 15);
+			this.pointDrawLabel.TabIndex = 4;
+			this.pointDrawLabel.Text = "Введите координаты";
+			this.pointDrawLabel.Click += new System.EventHandler(this.pointDrawLabel_Click);
 			// 
-			// textBox1
+			// panel
 			// 
-			this.textBox1.Location = new System.Drawing.Point(49, 10);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 23);
-			this.textBox1.TabIndex = 5;
+			this.panel.BackColor = System.Drawing.SystemColors.Control;
+			this.panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel.Controls.Add(this.yNLabel);
+			this.panel.Controls.Add(this.xNLabel);
+			this.panel.Controls.Add(this.yNTextBox);
+			this.panel.Controls.Add(this.xNTextBox);
+			this.panel.Controls.Add(this.yKLabel);
+			this.panel.Controls.Add(this.xKLabel);
+			this.panel.Controls.Add(this.lineBuildButton);
+			this.panel.Controls.Add(this.yKtextBox);
+			this.panel.Controls.Add(this.xKtextBox);
+			this.panel.Controls.Add(this.groupBox1);
+			this.panel.Controls.Add(this.pointDrawLabel);
+			this.panel.Controls.Add(this.clearButton);
+			this.panel.Location = new System.Drawing.Point(369, 28);
+			this.panel.Name = "panel";
+			this.panel.Size = new System.Drawing.Size(282, 358);
+			this.panel.TabIndex = 5;
 			// 
-			// firstMatrixStatusLabel
+			// yKLabel
 			// 
-			this.firstMatrixStatusLabel.AutoSize = true;
-			this.firstMatrixStatusLabel.Location = new System.Drawing.Point(155, 51);
-			this.firstMatrixStatusLabel.Name = "firstMatrixStatusLabel";
-			this.firstMatrixStatusLabel.Size = new System.Drawing.Size(68, 15);
-			this.firstMatrixStatusLabel.TabIndex = 6;
-			this.firstMatrixStatusLabel.Text = "Не создана";
+			this.yKLabel.AutoSize = true;
+			this.yKLabel.Location = new System.Drawing.Point(22, 219);
+			this.yKLabel.Name = "yKLabel";
+			this.yKLabel.Size = new System.Drawing.Size(37, 15);
+			this.yKLabel.TabIndex = 9;
+			this.yKLabel.Text = "Y кон";
 			// 
-			// secondMatrixStatusLabel
+			// xKLabel
 			// 
-			this.secondMatrixStatusLabel.AutoSize = true;
-			this.secondMatrixStatusLabel.Location = new System.Drawing.Point(155, 98);
-			this.secondMatrixStatusLabel.Name = "secondMatrixStatusLabel";
-			this.secondMatrixStatusLabel.Size = new System.Drawing.Size(68, 15);
-			this.secondMatrixStatusLabel.TabIndex = 7;
-			this.secondMatrixStatusLabel.Text = "Не создана";
+			this.xKLabel.AutoSize = true;
+			this.xKLabel.Location = new System.Drawing.Point(20, 177);
+			this.xKLabel.Name = "xKLabel";
+			this.xKLabel.Size = new System.Drawing.Size(37, 15);
+			this.xKLabel.TabIndex = 8;
+			this.xKLabel.Text = "X кон";
+			// 
+			// lineBuildButton
+			// 
+			this.lineBuildButton.Location = new System.Drawing.Point(104, 245);
+			this.lineBuildButton.Name = "lineBuildButton";
+			this.lineBuildButton.Size = new System.Drawing.Size(75, 40);
+			this.lineBuildButton.TabIndex = 7;
+			this.lineBuildButton.Text = "Построить отрезок";
+			this.lineBuildButton.UseVisualStyleBackColor = true;
+			this.lineBuildButton.Click += new System.EventHandler(this.lineBuildButton_Click);
+			// 
+			// yKtextBox
+			// 
+			this.yKtextBox.Location = new System.Drawing.Point(65, 216);
+			this.yKtextBox.Name = "yKtextBox";
+			this.yKtextBox.Size = new System.Drawing.Size(63, 23);
+			this.yKtextBox.TabIndex = 6;
+			// 
+			// xKtextBox
+			// 
+			this.xKtextBox.Location = new System.Drawing.Point(65, 174);
+			this.xKtextBox.Name = "xKtextBox";
+			this.xKtextBox.Size = new System.Drawing.Size(63, 23);
+			this.xKtextBox.TabIndex = 5;
+			// 
+			// yNLabel
+			// 
+			this.yNLabel.AutoSize = true;
+			this.yNLabel.Location = new System.Drawing.Point(162, 219);
+			this.yNLabel.Name = "yNLabel";
+			this.yNLabel.Size = new System.Drawing.Size(37, 15);
+			this.yNLabel.TabIndex = 13;
+			this.yNLabel.Text = "Y нач";
+			// 
+			// xNLabel
+			// 
+			this.xNLabel.AutoSize = true;
+			this.xNLabel.Location = new System.Drawing.Point(160, 177);
+			this.xNLabel.Name = "xNLabel";
+			this.xNLabel.Size = new System.Drawing.Size(37, 15);
+			this.xNLabel.TabIndex = 12;
+			this.xNLabel.Text = "X нач";
+			// 
+			// yNTextBox
+			// 
+			this.yNTextBox.Location = new System.Drawing.Point(205, 216);
+			this.yNTextBox.Name = "yNTextBox";
+			this.yNTextBox.Size = new System.Drawing.Size(63, 23);
+			this.yNTextBox.TabIndex = 11;
+			// 
+			// xNTextBox
+			// 
+			this.xNTextBox.Location = new System.Drawing.Point(205, 174);
+			this.xNTextBox.Name = "xNTextBox";
+			this.xNTextBox.Size = new System.Drawing.Size(63, 23);
+			this.xNTextBox.TabIndex = 10;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 183);
-			this.Controls.Add(this.secondMatrixStatusLabel);
-			this.Controls.Add(this.firstMatrixStatusLabel);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.matrixSizeLabel);
-			this.Controls.Add(this.saveResultButton);
-			this.Controls.Add(this.resultButton);
-			this.Controls.Add(this.createSecondMatrixButton);
-			this.Controls.Add(this.createFirstMartixButton);
+			this.ClientSize = new System.Drawing.Size(663, 398);
+			this.Controls.Add(this.panel);
+			this.Controls.Add(this.pictureBox);
 			this.Name = "MainForm";
 			this.Text = "Matrix calculator";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel.ResumeLayout(false);
+			this.panel.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Button createFirstMartixButton;
-		private System.Windows.Forms.Button createSecondMatrixButton;
-		private System.Windows.Forms.Button resultButton;
-		private System.Windows.Forms.Button saveResultButton;
-		private System.Windows.Forms.Label matrixSizeLabel;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Label firstMatrixStatusLabel;
-		private System.Windows.Forms.Label secondMatrixStatusLabel;
+		private System.Windows.Forms.Button clearButton;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton simpleCDARadioButton;
+		private System.Windows.Forms.Label pointDrawLabel;
+		private System.Windows.Forms.Panel panel;
+		private System.Windows.Forms.Button lineBuildButton;
+		private System.Windows.Forms.TextBox yKtextBox;
+		private System.Windows.Forms.TextBox xKtextBox;
+		private System.Windows.Forms.Label xKLabel;
+		private System.Windows.Forms.Label yKLabel;
+		private System.Windows.Forms.Label yNLabel;
+		private System.Windows.Forms.Label xNLabel;
+		private System.Windows.Forms.TextBox yNTextBox;
+		private System.Windows.Forms.TextBox xNTextBox;
 	}
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -10,11 +10,159 @@ using System.Windows.Forms;
 
 namespace ComputerGraphics
 {
+	// TODO: настройка цвета линии
+	// TODO: настройка стиля линии - пунктир, толстая, сплошная
+	// TODO: шаблоны отрезков/прямоугольников/треугольников/14 вариант
+	// TODO: генерация окружности по алгоритму Брезенхема
 	public partial class MainForm : Form
 	{
+		// начальные x y и конечные.
+		private double xn, yn, xk, yk;
+
+		private Pen _pen = new Pen(Color.Black, 1);
+
+		private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+		{
+			simpleCDADraw(e);
+		}
+
+		/// <summary>
+		/// Отрисвока линии алгоритмом обычный ЦДА.
+		/// </summary>
+		/// <param name="e"></param>
+		private void simpleCDADraw(MouseEventArgs e, double xk = 0, double yk = 0)
+		{
+			int i, n;
+			double xt, yt, dx, dy;
+
+			// используется рисование мышью
+			if (xk == 0 && yk == 0)
+			{
+				xk = e.X;
+				yk = e.Y;
+			}
+			
+			dx = xk - xn;
+			dy = yk - yn;
+			n = 100;
+			xt = xn;
+			yt = yn;
+
+			for (i = 1; i <= n; i++)
+			{
+
+				var g = Graphics.FromHwnd(pictureBox.Handle);
+
+				g.DrawRectangle(_pen, (int)xt, (int)yt, 2, 2);
+				//Рисуем закрашенный прямоугольник:
+				//Объявляем объект "redBrush", задающий цвет кисти
+				//SolidBrush redBrush = new SolidBrush(Color.Red);
+				//Рисуем закрашенный прямоугольник
+				//g.FillRectangle(redBrush, (int)xt, (int)yt, 2, 2);
+				xt = xt + dx / n;
+				yt = yt + dy / n;
+			}
+		}
+
+		private void clearButton_Click(object sender, EventArgs e)
+		{
+			pictureBox.Image = null;
+		}
+
+		private void pointDrawLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lineBuildButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				xk = Convert.ToDouble(xKtextBox.Text);
+				xn = Convert.ToDouble(xNTextBox.Text);
+				yk = Convert.ToDouble(yKtextBox.Text);
+				yn = Convert.ToDouble(yNTextBox.Text);
+
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				throw;
+			}
+
+			if (CheckPictureBoxSize(xk, yk) || CheckPictureBoxSize(xn, yn))
+			{
+				MessageBox.Show("Указанные координаты превосходят параметры элемента отображения",
+					"Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				xKtextBox.Text = String.Empty;
+				yKtextBox.Text = String.Empty;
+				yNTextBox.Text = "0";
+				xNTextBox.Text = "0";
+				return;
+			}
+			if (simpleCDARadioButton.Checked)
+			{
+				int i, n;
+				double xt, yt, dx, dy;
+
+				dx = xk - xn;
+				dy = yk - yn;
+				n = 100;
+				xt = xn;
+				yt = yn;
+
+				for (i = 1; i <= n; i++)
+				{
+
+					var g = Graphics.FromHwnd(pictureBox.Handle);
+
+					g.DrawRectangle(_pen, (int)xt, (int)yt, 2, 2);
+					//Рисуем закрашенный прямоугольник:
+					//Объявляем объект "redBrush", задающий цвет кисти
+					//SolidBrush redBrush = new SolidBrush(Color.Red);
+					//Рисуем закрашенный прямоугольник
+					//g.FillRectangle(redBrush, (int)xt, (int)yt, 2, 2);
+					xt = xt + dx / n;
+					yt = yt + dy / n;
+				}
+			}
+			else
+			{
+				MessageBox.Show("Выберите алгоритм", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+		}
+
+		private bool CheckPictureBoxSize(double x, double y)
+		{
+			if (pictureBox.Width < x || pictureBox.Height < y)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private void simpleCDARadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (simpleCDARadioButton.Checked)
+			{
+				xn = e.X;
+				yn = e.Y;
+			}
+
+			else MessageBox.Show("Вы не выбрали алгоритм вывода фигуры!",
+								"Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 		}
 	}
 }
